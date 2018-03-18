d55aaeaff9a86286f77cc9f3e8ab8b5bb157d3e1
fz <jsnjfz@gmail.com>
2018-03-18 21:42:48 +0800

    wechat author

---
+++ src/main/java/com/imooc/config/WechatAccountConfig.java
@@ -0,0 +1,55 @@
+package com.imooc.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @Author fz
+ * @Date 2018-03-17 21:20
+ *
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "wechat")
+public class WechatAccountConfig {
+
+    private String mpAppId;
+
+    private String mpAppSecret;
+
+    /**
+     * 开放平台id
+     */
+    private String openAppId;
+
+    /**
+     * 开放平台密钥
+     */
+    private String openAppSecret;
+
+    /**
+     * 商户号
+     */
+    private String mchId;
+
+    /**
+     * 商户密钥
+     */
+    private String mchKey;
+
+    /**
+     * 商户证书路径
+     */
+    private String keyPath;
+
+    /**
+     * 微信支付异步通知地址
+     */
+    private String notifyUrl;
+
+    private Map<String, String> templateId;
+
+}
